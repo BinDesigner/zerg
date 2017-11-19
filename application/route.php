3453879a45dc28e2e3525107b56580e2d48ff1b8
Bin <huangbindes@gmail.com>
2017-11-19 18:23:15 +0800

    branch 1尝试

--- application/route.php
+++ application/route.php
@@ -35,4 +35,4 @@ Route::get('api/:version/product/:id','api/:version.Product/getOne');
 
 Route::get('api/:version/category/all','api/:version.Category/getAllCategories');
 
-Route::post('api/:version/token/user','api/:version.Token/getToken');
+Route::post('api/:version/token/user0987','api/:version.Token/getToken');
