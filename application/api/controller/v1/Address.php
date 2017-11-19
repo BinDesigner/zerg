bfe221058ac778b545c5f3def15ce18daf8be69d
Bin <huangbindes@gmail.com>
2017-11-20 00:21:16 +0800

    9-12结束

---
+++ application/api/controller/v1/Address.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Machenike
+ * Date: 2017/11/20
+ * Time: 0:08
+ */
+
+namespace app\api\controller\v1;
+
+
+class Address
+{
+    public function createOrUpdateAddress()
+    {
+
+    }
+}
---
+++ application/api/validate/AddressNew.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Machenike
+ * Date: 2017/11/20
+ * Time: 0:12
+ */
+
+namespace app\api\validate;
+
+
+class AddressNew
+{
+    protected $rule = [
+        'name' =>'require|isNotEmpty',
+        'mobile' => 'require|isMobile',
+        'province' => 'require|isNotEmpty',
+        'city' => 'require|isNotEmpty',
+        'country' => 'require|isNotEmpty',
+        'detail' => 'require|isNotEmpty',
+        'uid' => 'require|isNotEmpty',
+    ];
+}
