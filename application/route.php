158b53c1634ceb488d30e039bbe9a1848da76d63
Bin <huangbindes@gmail.com>
2017-11-19 18:15:40 +0800

    三次尝试

--- application/route.php
+++ application/route.php
@@ -35,4 +35,4 @@ Route::get('api/:version/product/:id','api/:version.Product/getOne');
 
 Route::get('api/:version/category/all','api/:version.Category/getAllCategories');
 
-Route::post('api/:version/token/user12','api/:version.Token/getToken');
+Route::post('api/:version/token/user123','api/:version.Token/getToken');
