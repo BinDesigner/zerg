dc03d3f871ac6f4b0a5576ee014699e29cd61b9c
huangbindes <240148317@qq.com>
2017-12-08 18:38:47 +0800

    10-14(2)调试

--- application/api/service/Order.php
+++ application/api/service/Order.php
@@ -44,7 +44,9 @@ class Order
 
         // 开始创建订单
         $orderSnap = $this->snapOrder($status);
+
         $order = $this->createOrder($orderSnap);
+
         $order['pass'] = true;
         return $order;
     }
